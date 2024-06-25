5e3722bbcfe4cca8b4e864a0ec34f36bb703cf86
jaishriram1 <122771637+jaishriram1@users.noreply.github.com>
2024-06-25 22:39:50 +0530

    temp.java

--- temp.java
+++ temp.java
@@ -1,18 +1,13 @@
 import java.util.Scanner;
 
-public class TemperatureConverter {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the temp value: ");
+        System.out.println("Enter the temp value: ");
         double temp = sc.nextDouble();
-
-        System.out.print("Enter the original unit (Celsius, Fahrenheit, or Kelvin): ");
+        System.out.print("Enter your value either in Celsius, Fahrenheit, or Kelvin: ");
         String oUnit = sc.next().toLowerCase();
-
         double convertedToFahrenheit;
         double convertedToKelvin;
-
         switch (oUnit) {
             case "celsius":
                 convertedToFahrenheit = (temp * 9 / 5) + 32;
@@ -27,10 +22,9 @@ public class TemperatureConverter {
                 convertedToKelvin = temp;
                 break;
             default:
-                System.out.println("Invalid unit. Please enter Celsius, Fahrenheit, or Kelvin.");
+                System.out.println("Invalid value , Sorry!");
                 return;
         }
         System.out.println("Converted to Fahrenheit: " + convertedToFahrenheit);
         System.out.println("Converted to Kelvin: " + convertedToKelvin);
     }
-}
