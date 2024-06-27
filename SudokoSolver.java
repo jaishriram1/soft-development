2fbca50bbf6ff5feb00affd1c4be3793810ed7bc
jaishriram1 <122771637+jaishriram1@users.noreply.github.com>
2024-06-27 22:22:13 +0530

    Create SudokoSolver.java
    Regarding the  question.....
    Create a program that solves Sudoku puzzles automatically. The program should take an input grid representing an unsolved Sudoku puzzle and use an algorithm to fill in the missing numbers.
    It should use backtracking or other suitable techniques to explore possible solutions and find the correct arrangement of numbers for the puzzle. Once solved, the program should display the completed Sudoku grid.

---
+++ SudokoSolver.java
@@ -0,0 +1,101 @@
+public class SudokuSolver {
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            System.out.println("Sudoku solved successfully:");
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists for the given Sudoku.");
+        }
+    }
+
+    // Function to solve the Sudoku puzzle using backtracking
+    public static boolean solveSudoku(int[][] board) {
+        int[] emptyCell = findEmptyCell(board);
+        int row = emptyCell[0];
+        int col = emptyCell[1];
+
+        // If there are no empty cells, puzzle is solved
+        if (row == -1 && col == -1) {
+            return true;
+        }
+
+        // Try numbers from 1 to 9
+        for (int num = 1; num <= 9; num++) {
+            if (isValid(board, row, col, num)) {
+                board[row][col] = num;
+
+                if (solveSudoku(board)) {
+                    return true;
+                }
+
+                // Backtrack if the current number doesn't lead to a solution
+                board[row][col] = 0;
+            }
+        }
+
+        // No valid number found, backtrack
+        return false;
+    }
+
+    // Function to find an empty cell in the Sudoku board
+    public static int[] findEmptyCell(int[][] board) {
+        int[] cell = {-1, -1};
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 0) {
+                    cell[0] = i;
+                    cell[1] = j;
+                    return cell;
+                }
+            }
+        }
+
+        return cell;
+    }
+
+    // Function to check if a number can be placed in a specific cell
+    public static boolean isValid(int[][] board, int row, int col, int num) {
+        // Check row and column constraints
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 grid constraints
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // Function to print the Sudoku board
+    public static void printBoard(int[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
